8a7c634c37443592b4c22e3b80835697343ebf74
Kristian Kalvå <kkalvaa@gmail.com>
2023-06-15 22:38:43 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import ScoreComponent from './components/ScoreComponent.vue';
+
+interface Score {
+  category: string,
+  score: number,
+  icon: string
+}
+
+const scores: Score[] = [
+  {
+    category: 'Reaction',
+    score: 80,
+    icon: './assets/icon-reaction.svg'
+  },
+  {
+    category: 'Memory',
+    score: 92,
+    icon: './assets/icon-memory.svg'
+  },
+  {
+    category: 'Verbal',
+    score: 61,
+    icon: './assets/icon-verbal.svg'
+  },
+  {
+    category: 'Visual',
+    score: 72,
+    icon: './assets/icon-visual.svg'
+  }
+]
+</script>
+
+<template>
+  <main>
+    <div class="c-report-card">
+      <div class="c-report-card__section c-report-card__section--result">
+        <h1>Your Result</h1>
+        <div class="c-score-circle">
+          <strong class="c-score-circle__score">76</strong>
+          of 100
+        </div>
+        <strong>Great</strong>
+        <p>You scored higher than 65% of the people who have taken these tests.</p>
+      </div>
+      <div class="c-report-card__section c-report-card__section--summary">
+        <h2>Summary</h2>
+        <ScoreComponent
+          v-for="score in scores"
+          :key="score.category"
+          :score="score"
+        />
+        <button class="c-btn" type="button">Continue</button>
+      </div>
+    </div>
+  </main>
+</template>
---
+++ src/components/ScoreComponent.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+interface Score {
+  category: string,
+  score: number,
+  icon: string
+}
+defineProps<{
+  score: Score,
+}>();
+</script>
+
+<template>
+  <div
+    class="c-score"
+    :class="{
+      'is-reaction' : score.category === 'Reaction',
+      'is-memory' : score.category === 'Memory',
+      'is-verbal' : score.category === 'Verbal',
+      'is-visual' : score.category === 'Visual',
+    }"
+  >
+    <img :src="score.icon" alt="" class="c-score__image">
+    <p class="c-score__category">{{ score.category }}</p>
+    <p class="c-score__value">{{ score.score }} <span class="c-score__ref">/ 100</span></p>
+  </div>
+</template>
